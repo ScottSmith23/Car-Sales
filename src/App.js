ad4cc5e1feac7bc7d6a260e70fb66d03d599fee2
Scott Smith <scthsm@outlook.com>
2020-03-17 19:15:57 -0600

    using Redux hooks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
-import React from 'react';
-
+import React, { useState, useCallback } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
-import {connect} from 'react-redux';
+import {connect,useSelector, useDispatch} from 'react-redux';
 import {addFeature, removeFeature} from './actions/carActions'
+
 const App = (props) => {
-
-  // const removeFeature = item => {
-    
-  // };
-
-  // const buyItem = item => {
-  //   // dipsatch an action here to add an item
-  // };
+  //state redux hooks
+  const carData = useSelector(state => state.car)
+  const featureData = useSelector(state => state.additionalFeatures)
+  const priceData = useSelector(state => state.additionalPrice)
+  //dispatch redux hooks
+  const dispatch = useDispatch()
+  const addF = useCallback((e)=>dispatch(addFeature(e)),[dispatch])
+  const removeF = useCallback((e)=>dispatch(removeFeature(e)),[dispatch])
 
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={props.car} />
-        <AddedFeatures car={props.car} removeFeature={props.removeFeature} />
+        <Header car={carData} />
+        <AddedFeatures car={carData} removeFeature={removeF} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={props.additionalFeatures} addFeature={props.addFeature} />
-        <Total car={props.car} additionalPrice={props.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={featureData} addFeature={addF} />
+        <Total car={carData} additionalPrice={priceData} />
       </div>
     </div>
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    car: state.car,
-    additionalFeatures: state.additionalFeatures,
-    additionalPrice: state.additionalPrice
-  };
-};
+// const mapStateToProps = state => {
+//   return {
+//     car: state.car,
+//     additionalFeatures: state.additionalFeatures,
+//     additionalPrice: state.additionalPrice
+//   };
+// };
 
-export default connect(
-  mapStateToProps,
-  {addFeature, removeFeature}
+// export default connect(
+//   mapStateToProps,
+//   {addFeature, removeFeature}
   
-)(App);
+// )(App);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import {carReducer} from './reducers/carReducer';
@@ -11,8 +12,10 @@ const store = createStore(carReducer);
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
+  <Router>
     <Provider store={store}>
       <App />
-    </Provider>,
+    </Provider>
+    </Router>,
     rootElement
   );
